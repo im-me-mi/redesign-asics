8e2c1621f0b32a783b165129dfd4a813e16ec3a2
Miryeong Lee <lmryeong23@gmail.com>
2023-01-24 02:14:17 +0900

    bxslider수정

--- js/men.js
+++ js/men.js
@@ -1,16 +1,21 @@
 $(function () {
-    $('.dp_cate li').click(function(){
-       $(this).css("font-weight",500);
-    });
+  $(".dp_cate li a").click(function () {
+    if ($(this).hasClass("active")) {
+      $(".dp_cate li a").removeClass("active");
+    } else {
+      $(".dp_cate li a").removeClass("active");
 
-    $('.more_btn_wrap button').click(function(){
-        if($('.dp_wrap').css("height") < "4770px")
-        {
-            $('.dp_wrap').css("height","+=1590px");
-        };
+      $(this).addClass("active");
+    }
+    // let fontBold = $(this).css({ color: "#00116e", fontWeight: "500" });
+  });
+
+  $(".more_btn_wrap button").click(function () {
+    if ($(".dp_wrap").css("height") < "4770px") {
+      $(".dp_wrap").css("height", "+=1590px");
+    }
     // $('.dp_wrap').css({"height":"+=1600px"});
     //     if($('.dp_wrap').css("height") == "4800px")
     //     {$('.more_btn_wrap button').attr("disabled",true)};
-    });
-
+  });
 });
